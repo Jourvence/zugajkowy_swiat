2f4794b0eccb9c2068c81d70bee7bb901b0d6779
Jourvence <117796287+Vinqueire@users.noreply.github.com>
2024-06-25 23:50:38 +0200

    First commit
    essa

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    
+    <title>Document</title>
+</head>
+<body>
+    <main class="wrapper-main">
+    <header name="exp">zugaj <u>E X P I R I E N C E</u></header>
+
+    <button name="firstButton" onclick="firstScript()">:O</button>
+    <br>
+    <button name="secondButton" onclick="secondScript()">wild zugajka?</button>
+    <br><br>
+    <button name="thirdButton" onclick="thirdScript()">No way</button>
+    <br>
+    <button name="forthButton" onclick="forthScript()">fr fr?</button>
+    <br>
+    <button name="fifthButton" onclick="fifthScript()">green flag 😎</button>
+    <br>
+    <button name="sixthButton" onclick=sixthScript()>💞</button>
+
+    <img class="a" src="css/heart.jpg" name="serce">
+
+    <?php 
+
+    ?>
+    </main>
+</body>
+<script src="js/script.js"></script>
+</html>
---
+++ js/script.js
@@ -0,0 +1,31 @@
+function firstScript(){
+    document.querySelector('[name="firstButton"]').style.display = "none";
+    document.querySelector('[name="secondButton"]').style.display = "initial";
+}
+
+function secondScript(){
+    document.querySelector('[name="secondButton"]').style.display = "none";
+    document.querySelector('[name="thirdButton"]').style.display = "initial";
+}
+
+function thirdScript(){
+    document.querySelector('[name="thirdButton"]').style.display = "none";
+    document.querySelector('[name="forthButton"]').style.display = "initial";
+}
+
+function forthScript(){
+    document.querySelector('[name="forthButton"]').style.display = "none";
+    document.querySelector('[name="fifthButton"]').style.display = "initial";
+}
+
+function fifthScript(){
+    document.querySelector('[name="fifthButton"]').style.display = "none";
+    document.querySelector('[name="sixthButton"]').style.display = "initial";
+}
+
+function sixthScript(){
+    document.querySelector('[name="sixthButton"]').style.display = "none";
+    document.querySelector('body').style.backgroundImage="url(css/zugaj.jpg)";
+    document.querySelector('[name="exp"]').style.display = "none";
+    document.querySelector('[name="serce"]').style.display = "initial";
+}
